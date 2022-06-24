754ad79965f88dc487a69902a5e0aa6ef882be62
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-24 17:01:39 +0200

    Add & update backend mongoose-robots

--- backend/src/models/robot.model.ts
+++ backend/src/models/robot.model.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 import { mongoooseConnect } from '../db/mongoose.js';
 
-export interface iRobot {
-    id: string;
-    name: string;
-    image: string;
-    speed: number;
-    life: number;
-    born: string;
-}
+// export interface iRobot {
+//     id: string;
+//     name: string;
+//     image: string;
+//     speed: number;
+//     life: number;
+//     born: string;
+// }
 
 const robotSchema = new mongoose.Schema({
     id: String,
@@ -17,11 +17,8 @@ const robotSchema = new mongoose.Schema({
     speed: Number,
     life: Number,
     born: String,
-    title: { type: String, required: true },
-    author: mongoose.SchemaTypes.String, //para primitivos no hace falta, basta con el constructor de js
-    alreadyRead: { type: Boolean, default: false },
 });
 
 export const Robot = mongoose.model('Book', robotSchema);
 
-await mongoooseConnect(); // ES IMPORTANTE NO OLVIDAR ESTABLECER LA CONEXIÓN
+await mongoooseConnect();
