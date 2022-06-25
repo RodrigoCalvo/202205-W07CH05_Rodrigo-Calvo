ca86a293a1acac6fbf669087b4b51a3a3cb19013
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-25 11:45:59 +0200

    Add data model

---
+++ frontend/src/models/robot.test.ts
@@ -0,0 +1,17 @@
+import { Robot } from './robot';
+
+describe('Given the class CartProduct', () => {
+    describe('When we instantiate an object', () => {
+        test(`Then the created object should 
+        have the product and count properties:`, () => {
+            const obj = new Robot('', '', 0, 0, '');
+            expect(obj).toBeInstanceOf(Robot);
+            expect(obj).toHaveProperty('id');
+            expect(obj).toHaveProperty('name');
+            expect(obj).toHaveProperty('image');
+            expect(obj).toHaveProperty('speed');
+            expect(obj).toHaveProperty('life');
+            expect(obj).toHaveProperty('born');
+        });
+    });
+});
