945aed1809a1a35824d0cc412f375c08caafeb44
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-06-25 11:43:24 +0200

    Delete unnecessary files

--- frontend/src/reducers/cartProducts/cartProduct.action.types.ts
+++
@@ -1,6 +0,0 @@
-export enum actionTypes {
-    'cartProduct@add' = 'cartProduct@add',
-    'cartProduct@delete' = 'cartProduct@delete',
-    'cartProduct@update' = 'cartProduct@update',
-    'cartProduct@load' = 'cartProduct@load',
-}
