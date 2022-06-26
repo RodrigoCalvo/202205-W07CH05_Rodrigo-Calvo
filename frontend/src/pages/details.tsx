b3b61728d4ccc4408577fad648d2ae6456546961
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-06-26 19:38:00 +0200

    Add edit button to details page

--- frontend/src/pages/details.tsx
+++ frontend/src/pages/details.tsx
@@ -1,13 +1,15 @@
+import { SyntheticEvent } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { iStore } from '../store/store';
 
 export function Details({ id }: { id: string }) {
     const products = useSelector((store: iStore) => store.robots);
     const detailsProduct = products.find((item) => item._id === id);
     const navigate = useNavigate();
-    function handleClick() {
-        navigate(-1);
+    function handleClick(ev: SyntheticEvent) {
+        const eventTarget = ev.target as HTMLElement;
+        if (eventTarget.innerText === 'Volver') navigate('../');
     }
     const template = (
         <>
@@ -21,11 +23,14 @@ export function Details({ id }: { id: string }) {
                     <p>Resistencia: {detailsProduct.life}</p>
                     <p>Velocidad: {detailsProduct.speed}</p>
                     <p>Fecha de construcción: {detailsProduct.born}</p>
+                    <button onClick={handleClick}>Volver</button>
+                    <Link to="./edit">
+                        <button>Editar</button>
+                    </Link>
                 </>
             ) : (
                 'El producto con esa ID no está disponible actualmente'
             )}
-            <button onClick={handleClick}>Volver</button>
         </>
     );
     return template;
