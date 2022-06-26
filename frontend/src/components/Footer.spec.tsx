e5adc9f398341f9bbe055ddb463120c5b47433f4
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-06-26 16:49:24 +0200

    Fix typo on test

--- frontend/src/components/Footer.spec.tsx
+++ frontend/src/components/Footer.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { Footer } from './Footer';
 
-describe('Given the NotFound component', () => {
+describe('Given the Footer component', () => {
     describe('When calling it', () => {
         test('Then it should render', () => {
             render(<Footer />);
