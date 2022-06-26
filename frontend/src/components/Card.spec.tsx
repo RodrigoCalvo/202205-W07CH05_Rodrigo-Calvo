5279ea4e5dc342327c159834cbf485abb5dc2f8b
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2022-06-26 16:48:00 +0200

    Update Card test with delete button functionality

--- frontend/src/components/Card.spec.tsx
+++ frontend/src/components/Card.spec.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { iRobot } from '../models/robot';
 import { robotReducer } from '../reducers/robots/robot.reducer';
 import { iStore } from '../store/store';
-import { render, screen } from '../utils/test-utils';
+import { fireEvent, render, screen } from '../utils/test-utils';
 import { RobotCard } from './Card';
+import { RobotHttpStore } from '../services/robot.http.store';
+
+jest.mock('../services/robot.http.store');
 
 const reducer = {
     robots: robotReducer,
-};
-const preloadedState: iStore = {
-    robots: [] as Array<iRobot>,
 };
 const mockItem1: iRobot = {
     _id: '',
@@ -19,13 +19,8 @@ const mockItem1: iRobot = {
     life: 0,
     born: '',
 };
-const mockItem2: iRobot = {
-    _id: '',
-    name: 'test2',
-    image: '',
-    speed: 0,
-    life: 0,
-    born: '',
+const preloadedState: iStore = {
+    robots: [] as Array<iRobot>,
 };
 
 describe('Given the Card component', () => {
@@ -41,5 +36,20 @@ describe('Given the Card component', () => {
             expect(element).toBeInTheDocument();
         });
     });
-    describe('When clicking on add button on a item not in the cart', () => {});
+    describe('When clicking on delete button', () => {
+        test('Then http.deleteRobot function should be called', () => {
+            const mockDelete = jest.fn();
+            mockDelete.mockResolvedValue({});
+            RobotHttpStore.prototype.deleteRobot = mockDelete;
+            render(
+                <BrowserRouter>
+                    <RobotCard robot={mockItem1} />
+                </BrowserRouter>,
+                { preloadedState, reducer }
+            );
+            const button = screen.getByRole('button');
+            fireEvent.click(button);
+            expect(mockDelete).toHaveBeenCalled();
+        });
+    });
 });
